e9a19b8d0b44895646fc6a6ef34830ab4266edbe
Srinivasan <srinivasankannan98@gmail.com>
2021-06-15 14:07:01 +0530

    Add files via upload

---
+++ alamoutiEncoder2x2MIMO.m
@@ -0,0 +1,52 @@
+function encodedMatrix = alamoutiEncoder2x2MIMO(inp,timeSlot)
+
+%Input 
+%inp is expected either be a 1x2 vector(for encoding data symbols) or 2x2
+%matrix (for encoding the channel matrix)
+%TimeSlot defines the time slot in which the data symbols are being sent
+
+%Output
+%encodedMatrix is a 2x1 vector in case of data symbol encoding and a 2x2
+%matrix in case of encoding of the channel matrix
+
+    temp = zeros(size(inp));
+
+    if(size(inp,1) == 1) %Encoding data
+
+        %Encoding the data symbols from each antenna for different time
+        %instances
+
+        if(timeSlot == 2)
+            temp(:,1) = -conj(inp(:,2));
+            temp(:,2) = conj(inp(:,1));
+
+        end
+
+        encodedMatrix = temp(:);
+
+    else
+        %Encoding of the channel matrix in order to compute the composite
+        %channel matrix
+        if(timeSlot == 2)
+%            temp(1,1) = inp(1,1);
+%            temp(1,2) = inp(1,2);
+%           temp(2,1) = conj(inp(1,2));
+%           temp(2,2) = -conj(inp(1,1));
+            temp(:,1) = conj(inp(:,2));
+            temp(:,2) = -conj(inp(:,1));
+        
+%        if(timeSlot == 2)
+%            temp(1,1) = inp(2,1);
+%            temp(1,2) = inp(2,2);
+%            temp(2,1) = conj(inp(2,2));
+%            temp(2,2) = -conj(inp(2,1));
+
+
+        end
+
+        encodedMatrix = temp;
+
+    end
+
+end
+    
